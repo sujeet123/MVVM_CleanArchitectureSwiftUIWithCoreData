1956c027cfe2b9729014bba36282c26985ab6e8e
Sujeet Kumar <sujeetkumartiwarry@gmail.com>
2026-09-14 11:41:29 +0530

    Initial Commit

---
+++ MVVM_CleanArchitectureSwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MVVM_CleanArchitectureSwiftUI
+//
+//  Created by Sujeet kumar on 14/09/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
